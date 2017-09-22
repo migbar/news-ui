63ca4431bd077bf41badba343cbb885b1dc5778c
Miguel Barcos <miguel@galar.com>
2017-09-22 14:46:19 +0200

    improve header aesthetics

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   beforeModel() {
-    this.transitionTo('latest');
+    console.log('in replace');
+    this.replaceWith('latest');
   }
 });
