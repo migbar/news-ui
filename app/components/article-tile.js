d12f16deb1dd96b23ee761a597438ef92fbcfdb5
Miguel Barcos <miguel@galar.com>
2017-10-30 11:03:52 -0400

    mainImage from post

--- app/components/article-tile.js
+++ app/components/article-tile.js
@@ -1,11 +1,8 @@
 import Ember from 'ember';
-const { computed, inject } = Ember;
+const { computed: { alias }, inject } = Ember;
 
 export default Ember.Component.extend({
-  imageGenerator: inject.service('image-url-generator'),
   tagName: 'article',
   classNames: ['bb b--black-05'],
-  mainImage: computed('article.mainImage', function() {
-    return this.get('imageGenerator').nextUrl();
-  })
+  mainImage: alias('article.thread.mainImage')
 });
