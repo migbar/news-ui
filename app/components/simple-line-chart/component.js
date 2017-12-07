5aba20efa4e60a449e0821144cc173ec766a6001
Miguel Barcos <miguel@galar.com>
2017-12-07 13:16:50 -0500

    generate data through ember-concurrency in component

--- app/components/simple-line-chart/component.js
+++ app/components/simple-line-chart/component.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
+import { task, timeout } from 'ember-concurrency';
+
 import { select } from 'd3-selection';
 import { range } from 'd3-array';
 import { scaleLinear, scaleTime } from 'd3-scale';
@@ -14,49 +16,60 @@ export default Ember.Component.extend({
   height: 200,
   globalX: 0,
   data: [],
-  duration: 700,
   max: 200,
   step: 2,
 
+  svg: null,
+  xScale: null,
+  yScale: null,
+  path: null,
+  smoothLine: null,
+
+  dataGeneratorTask: task(function * () {
+    while (true) {
+      // events come in at random intervals
+      let interval = (Math.random() * 350) + 150;
+      this.set('interval', interval);
+
+      yield timeout(interval);
+
+      let point = {
+        x: this.get('globalX'),
+        y: ((Math.random() * 70) + (Math.random() * 25) >> 0)  // y is a random value between 25 and 75
+      }
+
+      this.get('data').push(point);
+    }
+  }),
+
   didInsertElement() {
     this._super(...arguments);
-    this.set('xScale', scaleLinear().range([0, this.get('width')]));
-    this.set('yScale', scaleLinear().range([this.get('height'), 0]).domain([0, 100]));
 
+
+    this.set('svg', select(this.$('svg')[0])
+                        .attr('width', this.get('width'))
+                        .attr('height', this.get('height')));
+
+    this.set('xScale', scaleLinear()
+                        .range([0, this.get('width')]));
+
+    this.set('yScale', scaleLinear()
+                        .range([this.get('height'), 0])
+                        .domain([0, 100]));
+
+    this.set('path', this.get('svg')
+                        .append('path').style('stroke', 'teal')
+                        .attr('fill', 'none'));
+
+    this.set('smoothLine', line().curve(curveMonotoneX)
+                        .x(d => this.get('xScale')( d.x )+10)
+                        .y(d => this.get('yScale')( d.y )));
+
+    this.get('dataGeneratorTask').perform();
     this.renderChart();
   },
 
   renderChart() {
-    let svg = select(this.$('svg')[0])
-      .attr('width', this.get('width'))
-      .attr('height', this.get('height'));
-
-    let smoothLine = line().curve(curveMonotoneX)
-      .x(d => this.get('xScale')( d.x )+10)
-      .y(d => this.get('yScale')( d.y ));
-
-    let path = svg
-      .append('path')
-      .style('stroke', 'teal')
-      .attr('fill', 'none');
-
-    this.set('smoothLine', smoothLine);
-    this.set('path', path);
-
-    this.tick();
-  },
-
-  newPoint() {
-    console.log('once');
-    let y = ((Math.random() * 70) + (Math.random() * 25) >> 0);  // y is a random value between 25 and 75
-    let x = this.get('globalX');
-
-    return { x, y };
-  },
-
-  tick() {
-    console.log('here');
-    this.get('data').push(this.newPoint());
     this.set('globalX', this.get('globalX') + this.get('step'));
 
     this.get('xScale').domain([ this.get('globalX') - (this.get('max') - this.get('step')), this.get('globalX') ]);
@@ -65,18 +78,18 @@ export default Ember.Component.extend({
     this.get('path').datum(this.get('data')).attr('d', this.get('smoothLine'));
 
     // Shift the chart left
-    this.get('path').attr('transform', null)
+    this.get('path')
+      .attr('transform', null)
       .transition()
-      .duration(this.get('duration'))
+      .duration(this.get('interval'))
       .ease(easeLinear)
       .attr('transform', 'translate(' + this.get('xScale')(this.get('globalX') - this.get('max')) + ')')
-      .on('end', this.tick.bind(this));
+      .on('end', this.renderChart.bind(this));
 
     // Remove old this.get('data') if more points than max
     if (this.get('data').length > this.get('max')) {
       this.get('data').shift();
     }
   }
-
 
 });
