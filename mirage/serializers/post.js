3f7831f08a2e66e8f017952c333dd0dfa3dccbba
Miguel Barcos <miguel@galar.com>
2018-01-08 22:36:23 +0100

    selectively disable eslint for the include option of the mirage post serializer

--- mirage/serializers/post.js
+++ mirage/serializers/post.js
@@ -1,7 +1,14 @@
 import ApplicationSerializer from './application';
 
 export default ApplicationSerializer.extend({
-  include: ['thread'],
+
+  /* eslint-disable */
+  include: ['thread']
+  /* eslint-enable */
+
+  //
+  // following ember-eslint suggestion does not work for Mirage 'include'
+  //
   // init() {
   //   this._super(...arguments);
   //   this.include = ['thread'];
