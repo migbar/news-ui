c4ebcde315339301190d6d4f63b0b063b03cb1c7
Miguel Barcos <miguel@galar.com>
2018-01-08 23:08:41 +0100

    ember-cli-update to 2.18

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,32 @@ module.exports = {
   rules: {
     "no-console": "off",
     "no-debugger": "off"
-  }
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        'testem.js',
+        'ember-cli-build.js',
+        'config/**/*.js'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      }
+    },
+
+    // test files
+    {
+      files: ['tests/**/*.js'],
+      excludedFiles: ['tests/dummy/**/*.js'],
+      env: {
+        embertest: true
+      }
+    }
+  ]
 };
